60cf146bee9318262561a4ccf0dedcb23b7947e1
jak36348 <120121076+jak36348@users.noreply.github.com>
2024-02-25 18:40:47 -0500

    Add files via upload

---
+++ csci4370project2/src/Query2.java
@@ -0,0 +1,63 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Query2 {
+
+    public static void main(String[] args) {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+
+        try {
+
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/employees", "root", "Jakapple19");
+
+
+            String query = "SELECT e.first_name, e.last_name, de.from_date, de.to_date " +
+                    "FROM employees e " +
+                    "JOIN dept_manager de ON e.emp_no = de.emp_no " +
+                    "WHERE de.to_date != '9999-01-01' " + // Exclude managers with indefinite office tenure
+                    "ORDER BY DATEDIFF(de.to_date, de.from_date) DESC LIMIT 1";
+
+
+            preparedStatement = connection.prepareStatement(query);
+
+
+            resultSet = preparedStatement.executeQuery();
+
+
+            if (resultSet.next()) {
+                String firstName = resultSet.getString("first_name");
+                String lastName = resultSet.getString("last_name");
+                String fromDate = resultSet.getString("from_date");
+                String toDate = resultSet.getString("to_date");
+
+
+                System.out.println("Manager(s) who hold office for the longest duration:");
+                System.out.println("Name: " + firstName + " " + lastName);
+                System.out.println("From Date: " + fromDate);
+                System.out.println("To Date: " + toDate);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+
+            try {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
